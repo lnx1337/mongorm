dc9ab49fa1cb6f74b2f57b973ddb7769521e3c30
lnx1337 <dev.lnx1337@gmail.com>
2015-11-13 18:32:34 -0600

    mongorm

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"os"
+)
+
+var session *mgo.Session
+var Db string
+var Col string
+
+func InitDb() {
+
+	var err error
+
+	MONGO_HOST := os.Getenv("MONGO_HOST")
+	MONGO_USER := os.Getenv("MONGO_USER")
+	MONGO_PASS := os.Getenv("MONGO_PASS")
+	MONGO_PORT := os.Getenv("MONGO_PORT")
+
+	urlConection := fmt.Sprintf("mongodb://%s:%s@%s:%s", MONGO_USER, MONGO_PASS, MONGO_HOST, MONGO_PORT)
+
+	session, err = mgo.Dial(urlConection)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	return
+}
+
+func Sess() *mgo.Session {
+	return session
+}
+
+func Collection() (col *mgo.Collection, err error) {
+	col = session.DB(Db).C(Col)
+	return col, err
+}
---
+++ orm/orm.go
@@ -0,0 +1,205 @@
+package orm
+
+import (
+	conn "appworkz/mongo/config"
+	"fmt"
+	"github.com/astaxie/beego/validation"
+	"github.com/lnx1337/gobase/base/api"
+	util "github.com/lnx1337/gobase/base/util"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"reflect"
+	"strings"
+)
+
+type Orm struct {
+	Model         interface{}
+	PkStructField string
+	PkBson        string
+}
+
+var Collection *mgo.Collection
+
+func NewOrm(model interface{}) Orm {
+	var err error
+	self := Orm{Model: model}
+	val := reflect.ValueOf(model)
+
+	conn.Db = self.GetModel("DbName", val)
+	conn.Col = self.GetModel("CollectionName", val)
+
+	conn.InitDb()
+
+	Collection, err = conn.Collection()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return self
+}
+
+func (self *Orm) SetPK(structField string, bsonTag string) {
+	self.PkStructField = structField
+	self.PkBson = bsonTag
+}
+
+func (self *Orm) getPkValue() interface{} {
+	var pkValue interface{}
+	if len(self.PkStructField) == 0 {
+		self.PkBson = "_id"
+		pkValue = reflect.ValueOf(self.Model).Elem().FieldByName("Id").Interface().(bson.ObjectId)
+	} else {
+		pkValue = reflect.ValueOf(self.Model).Elem().FieldByName(self.PkStructField).Interface().(string)
+	}
+	return pkValue
+}
+
+// TODO:
+// FindAll
+// map[string]interface{}
+// Sort()
+
+func (self *Orm) FindById(id string) (interface{}, *api.Err) {
+	errors := api.NewError()
+	err := Collection.Find(
+		bson.M{
+			self.PkBson: id,
+		},
+	).One(self.Model)
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoSelectAll,
+			17,
+		)
+	}
+	return self.Model, errors
+}
+
+func (self *Orm) FindByCondition(model interface{}, cond interface{}) *api.Err {
+	errors := api.NewError()
+	err := Collection.Find(cond).All(model)
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoSelectAll,
+			17,
+		)
+	}
+	return errors
+}
+
+func (self *Orm) FindByPk() (interface{}, *api.Err) {
+	errors := api.NewError()
+	id := self.getPkValue()
+	err := Collection.Find(
+		bson.M{
+			self.PkBson: id,
+		},
+	).One(self.Model)
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoSelectAll,
+			17,
+		)
+	}
+	return self.Model, errors
+}
+
+func (self *Orm) Save() *api.Err {
+	errors := api.NewError()
+	err := Collection.Insert(self.Model)
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoInsert,
+			18,
+		)
+	}
+	return errors
+}
+
+func (self *Orm) Update() *api.Err {
+	errors := api.NewError()
+
+	data, _ := util.ToMap(self.Model, "json")
+
+	pkValue := self.getPkValue()
+
+	err := Collection.Update(
+		bson.M{
+			self.PkBson: pkValue,
+		},
+		bson.M{
+			"$set": data,
+		},
+	)
+
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoUpdate,
+			19,
+		)
+	}
+	return errors
+}
+
+func (self *Orm) Delete() *api.Err {
+	errors := api.NewError()
+	id := self.getPkValue()
+	err := Collection.Remove(
+		bson.M{
+			self.PkBson: id,
+		},
+	)
+	if err != nil {
+		errors.SetErr(
+			err.Error(),
+			api.ErrMongoDelete,
+			20,
+		)
+	}
+	return errors
+}
+
+func (self *Orm) Validate() *api.Err {
+	errors := api.NewError()
+	valid := validation.Validation{}
+	b, _ := valid.Valid(self.Model)
+	if !b {
+		for _, err := range valid.Errors {
+			errors.SetErr(err.Key, err.Message, 0)
+		}
+	}
+	return errors
+}
+
+func (self *Orm) GetModel(nameMethod string, val reflect.Value) string {
+	ind := reflect.Indirect(val)
+	fun := val.MethodByName(nameMethod)
+	if fun.IsValid() {
+		vals := fun.Call([]reflect.Value{})
+		if len(vals) > 0 {
+			val := vals[0]
+			if val.Kind() == reflect.String {
+				return val.String()
+			}
+		}
+	}
+	return self.snakeString(ind.Type().Name())
+}
+
+func (self *Orm) snakeString(s string) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	for i, d := range s {
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		j = (d != '_')
+		data = append(data, byte(d))
+	}
+	return strings.ToLower(string(data[:len(data)]))
+}
